0fc6f9d8147f407cdfbadd042e5f006cdb7a63cf
KLazarovska <kika.lazarovska@gmail.com>
2016-04-21 14:03:06 +0200

    QuotientAndRemainder - language elements exercise

---
+++ QuotientAndRemainder/src/procedural/QuotientAndRemainder.java
@@ -0,0 +1,18 @@
+package procedural;
+
+public class QuotientAndRemainder {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		int number1 = 10;
+		int number2 = 5;
+		
+		System.out.println("Number 1 = " + number1 + "\n" + "Number 2 = " + number2);
+		System.out.println("Quotient = " + number1 / number2 + "\t" + "Remainder = " + number1 % number2);
+
+	}
+
+}
